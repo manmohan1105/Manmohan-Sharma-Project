b3ab09bd9ed71d32182f6161ac30cda445793dc6
manmohan1105 <manmohan1105@gmail.com>
2022-01-23 02:19:35 +0530

    commit2

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 import os
-from flask import Flask
+from urllib import request
+from flask import Flask ,redirect,url_for,request
 from flask import render_template
 
 # creates a Flask application, named app
 app = Flask(__name__)
 
-
-@app.route("/",methods=['GET'])
+@app.route('/login',methods=['POST','GET'])
 def Login():
-   return render_template('Login.html')
+   if request.method=='POST':
+       print("Login")
+       return render_template('Login.html')
+   else:    
+         return render_template('Login.html')
 
 
-@app.route("/register",methods=['GET'])
+
+
+
+
+@app.route('/register',methods=['GET','POST'])
 def Register():
-   return render_template('Register.html')
+   if request.method=='POST':
+       return render_template('Register.html')
+   else:    
+         return render_template('Register.html')
 
 
-@app.route("/dashboard",methods=['GET'])
+
+
+@app.route('/dashboard',methods=['GET','POST'])
 def Dashboard():
-   return render_template('Dashboard.html')
+   if request.method=='POST':
+       return render_template('Login.html')
+   else:    
+         return render_template('Dashboard.html')
+
+
 
 # run the application
 if __name__ == "__main__":
